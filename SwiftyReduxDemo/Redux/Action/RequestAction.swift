5cd12d1e4e3d01100e3d0ae0e9b161b963ae020d
max kruchkov <max.kryuchkov111@gmail.com>
2022-08-03 20:35:57 +0300

    test files removed, async await network request added

--- SwiftyReduxDemo/Redux/Action/RequestAction.swift
+++ SwiftyReduxDemo/Redux/Action/RequestAction.swift
@@ -6,20 +6,23 @@
 //
 
 import SwiftyRedux
+import Foundation
 
 protocol RequestAction: Action {}
 
 struct RequestActions {
     
     struct GetData: RequestAction {
-        
         func thunk() -> Thunk<AppState> {
             return Thunk { dispatch, getState in
                 guard let _ = getState() else {
                     return
                 }
                 dispatch(DidStart())
-                Application.networkManager.getTestData { result in
+                Task { @MainActor in
+                    print("🚀ON1 queue: \(String(cString: __dispatch_queue_get_label(nil), encoding: .utf8) ?? "??")")
+                    let result = await Application.networkManager.getTestDataUsingAsyncAwait()
+                    print("🚀ON2 queue: \(String(cString: __dispatch_queue_get_label(nil), encoding: .utf8) ?? "??")")
                     switch result {
                     case .success(let posts):
                         dispatch(DidFinish(posts: posts))
--- SwiftyReduxDemo/Utilities/NetworkManager.swift
+++ SwiftyReduxDemo/Utilities/NetworkManager.swift
@@ -12,9 +12,33 @@ final class NetworkManager {
     
     private var cancellables = Set<AnyCancellable>()
     
-    func getTestData(completion: @escaping (Result<[Post], Error>) -> Void) {
+    private func perform(request _request: URLRequest) async throws -> Data {
+        let session = URLSession(configuration: .default)
+        let (data, _) = try await session.data(for: _request)
+        return data
+    }
+    
+    func getTestDataUsingAsyncAwait() async -> Result<[Post], Error> {
         let urlString = "https://jsonplaceholder.typicode.com/posts"
+        guard let url = URL(string: urlString) else {
+            return .failure(NetworkManagerError.invalidRequest)
+        }
         
+        var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 100)
+        request.httpMethod = "GET"
+        
+        do {
+            let data = try await perform(request: request)
+            let posts = try JSONDecoder().decode([Post].self, from: data)
+            return .success(posts)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func getTestDataUsingCombine(completion: @escaping (Result<[Post], Error>) -> Void) {
+        let urlString = "https://jsonplaceholder.typicode.com/posts"
+
         guard let requestURL = URL(string: urlString) else {
             completion(.failure(NetworkManagerError.invalidRequest))
             return
@@ -28,8 +52,8 @@ final class NetworkManager {
                 return data
             })
             .decode(type: [Post].self, decoder: JSONDecoder())
-            .sink(receiveCompletion: { asd in
-                switch asd {
+            .sink(receiveCompletion: { result in
+                switch result {
                 case.failure(let error):
                     completion(.failure(error))
                 case .finished:
